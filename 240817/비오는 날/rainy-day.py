b73a519078ecb91df3d48375439e91c0a91e0c75
tydfuyhf <allen516@khu.ac.kr>
2024-08-17 16:31:10 +0900

    Title: 비오는 날, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240817/비오는 날/rainy-day.py
+++ 240817/비오는 날/rainy-day.py
@@ -1,13 +1,7 @@
-n=int(input())
-fin=[]
-ans=[]
-for _ in range(n):
-    x=list(input().split())
-    if x[2]=='Rain':
-        fin.append(x)
-        y=list(x[0].split("-"))
-        ans.append(y[0])
+n = int(input())
+info = sorted([list(map(str, input().split())) for _ in range(n)])
 
-minindex=ans.index(min(ans))
-for i in fin[minindex]:
-    print(i,end=" ")
+for i in info:
+    if i[2] == 'Rain':
+        print(' '.join(i))
+        break
