ff08ab9b81d419f4610b3787cf5a7f72e84b390d
tydfuyhf <allen516@khu.ac.kr>
2024-08-27 23:40:17 +0900

    Title: 줄 세우기 2, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240827/줄 세우기 2/line-up-students-2.py
@@ -0,0 +1,12 @@
+n=int(input())
+x=[]
+cnt=0
+for _ in range(n):
+    cnt+=1
+    y=tuple(map(int,input().split()))
+    y=y+(cnt,)
+    x.append(y)
+x.sort(key=lambda x: (x[0],x[1]))
+for i in x:
+    a,b,c=i
+    print(a,b,c)
