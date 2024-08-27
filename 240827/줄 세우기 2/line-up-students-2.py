76437adde9b53da36a147d53503d11b20ffcd326
tydfuyhf <allen516@khu.ac.kr>
2024-08-27 23:41:13 +0900

    Title: 줄 세우기 2, Time: 146ms, Memory: 25MB, Status: Passed - CodeTree

--- 240827/줄 세우기 2/line-up-students-2.py
+++ 240827/줄 세우기 2/line-up-students-2.py
@@ -6,7 +6,7 @@ for _ in range(n):
     y=tuple(map(int,input().split()))
     y=y+(cnt,)
     x.append(y)
-x.sort(key=lambda x: (x[0],x[1]))
+x.sort(key=lambda x: (x[0],-x[1]))
 for i in x:
     a,b,c=i
     print(a,b,c)
