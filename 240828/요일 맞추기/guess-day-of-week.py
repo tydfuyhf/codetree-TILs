6773616c36717542d44aa092d21d50d6861d0ff0
tydfuyhf <allen516@khu.ac.kr>
2024-08-28 21:45:45 +0900

    Title: 요일 맞추기, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240828/요일 맞추기/guess-day-of-week.py
+++ 240828/요일 맞추기/guess-day-of-week.py
@@ -4,9 +4,9 @@ m1,d1,m2,d2=map(int,input().split())
 time1=d1
 time2=d2
 for i in range(1,m1):
-    time1+=i
+    time1+=num_of_days[i]
 for i in range(1,m2):
-    time2+=i
+    time2+=num_of_days[i]
 
     
 differ=(time2-time1)%7
