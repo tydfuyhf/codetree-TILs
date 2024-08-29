d9cfa55267668bd4eaef71802ecbe292134c911c
tydfuyhf <allen516@khu.ac.kr>
2024-08-29 19:11:15 +0900

    Title: 그 요일은, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 240829/그 요일은/the-day-of-the-day.py
+++ 240829/그 요일은/the-day-of-the-day.py
@@ -1,5 +1,5 @@
 days=["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
-num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+num_of_days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 m1,d1,m2,d2=map(int,input().split())
 A=str(input())
 time1=d1
@@ -8,7 +8,7 @@ for i in range(1,m1):
     time1+=num_of_days[i]
 for i in range(1,m2):
     time2+=num_of_days[i]
-differ=time2-time1+1
+differ=time2-time1
 index=days.index(A)
 cnt=0
 while differ>=index:
