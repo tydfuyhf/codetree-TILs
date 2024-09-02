a40b49425d180515e8ca615afd68b77247027074
박상현 <allen516@khu.ac.kr>
2024-09-02 20:02:46 +0900

    Title: 흰검 칠하기, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240902/흰검 칠하기/painting-white-black.py
+++ 240902/흰검 칠하기/painting-white-black.py
@@ -1,35 +1,37 @@
-n=int(input())
-limit=0
-collect=[]
-#왼쪽 흰색 오른쪽 검은색 / 2번 이상 칠해지면 회색으로 
+n = int(input())
+commands = []
+
+# 명령어를 입력받아 저장
 for _ in range(n):
-    x,d=input().split()
-    x=int(x)
-    limit+=x
-    collect.append((x,d))
-loc=limit
-cnt=[0 for _ in range(limit*2+1)]
-color=["" for _ in range(limit*2+1)]
-for x,d in collect:
-    # black
-    if d=="R":
-        for i in range(loc,loc+x):
-            if cnt[i]==0 or cnt[i]==1:
-                color[i]="B"
-                cnt[i]+=1
-            else:
-                color[i]="G"
-        loc+=x
-    #white
-    else:
-        for i in range(loc-x,loc):
-            if cnt[i]==0 or cnt[i]==1:
-                color[i]="W"
-            else:
-                color[i]="G"
-                cnt[i]+=1
-        loc-=x
-a=color.count("W")
-b=color.count("B")
-c=color.count("G")
-print(a,b,c)
+    x, d = input().split()
+    x = int(x)
+    commands.append((x, d))
+
+size = 10000  # 충분히 큰 타일 배열
+loc = size // 2  # 초기 위치를 중간으로 설정
+color = [""] * size  # 타일 배열 초기화
+cnt = [0] * size  # 각 타일이 몇 번 칠해졌는지 추적
+
+for x, d in commands:
+    if d == "R":
+        for i in range(loc, loc + x):
+            if cnt[i] == 0:  # 처음 칠할 때
+                color[i] = "B"
+            elif cnt[i] == 1 and color[i] == "W":  # 두 번째 칠할 때
+                color[i] = "G"
+            cnt[i] += 1
+        loc += x  # 오른쪽으로 이동
+    else:  # "L"
+        for i in range(loc - x, loc):
+            if cnt[i] == 0:  # 처음 칠할 때
+                color[i] = "W"
+            elif cnt[i] == 1 and color[i] == "B":  # 두 번째 칠할 때
+                color[i] = "G"
+            cnt[i] += 1
+        loc -= x  # 왼쪽으로 이동
+
+# 색깔별 타일 개수 계산
+a = color.count("W")
+b = color.count("B")
+c = color.count("G")
+print(a, b, c)
